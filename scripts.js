ccc75c1ce0843ce7591ec36bd4ce1537e88fa0da
Admin ASINF admin <admin@COHABSANTISTA.local>
2022-08-08 14:58:03 -0300

    feat: End of the project

--- scripts.js
+++ scripts.js
@@ -1,10 +1,51 @@
-const toggleElements = document.querySelectorAll('[data-switch="white"]');
-const btnSwitch = document.querySelector('.theme');
-function toggle(event) {
-  event.preventDefault();
-  toggleElements.forEach((element) => element.classList.toggle('active'));
+// função para trocar o icone ou qualque imagem no toggle dos elementos.
+function changeImage(element, targetIcon,className, src, pastSrc) {
+  if (element.classList.contains(className))
+  targetIcon.src = src
+  else
+  targetIcon.src = pastSrc
 }
 
-const events = ['click' , 'touchstart'];
+// Theme switch toggle function
+function initSwitchTheme() {
+  const header = document.querySelector('.header')
+  const mobileRocketLogo = document.querySelector('.logo-mobile');
+  const toggleElements = document.querySelectorAll('[data-switch="white"]');
+  // Verifica se existe os atributos data para que não trave o script da página caso não tenha.
+  if(toggleElements) {
+    const btnSwitch = document.querySelector('.theme');
 
-events.forEach((events) => btnSwitch.addEventListener(events, toggle));
+// Previne o padrão para quando for usado o touch não aconteca o bug de abrir e fechar 
+    function toggle(event) {
+      event.preventDefault();
+      toggleElements.forEach((element) => element.classList.toggle('active'));
+      changeImage(header ,mobileRocketLogo,'active','./img/logo-mobile-white.svg', './img/logo-mobile.svg');
+    }
+
+// Adiciona o evento de click e touchStart
+    const events = ['click' , 'touchstart'];
+    events.forEach((events) => btnSwitch.addEventListener(events, toggle));
+  }
+}
+initSwitchTheme();
+
+// Menu toggle function
+function initToggleMenu () {
+  const btnMenu = document.querySelector('.navigation-mobile');
+  if (btnMenu) {
+    const UlNavigation = document.querySelector('nav > ul');
+    const UlIcone = document.querySelector('nav > img');
+
+// open e close do menu trocando a imagem do Icone.
+    function toggleMenu(event) {
+      event.preventDefault();
+      UlNavigation.classList.toggle('mobile');
+      changeImage(UlNavigation ,UlIcone,'mobile','./img/menu-buguer-close.svg', './img/menu-buguer-open.svg');
+    }
+
+// Adiciona o evento de click e touchStart
+    const events = ['click' , 'touchstart'];
+    events.forEach((eventos) => btnMenu.addEventListener(eventos, toggleMenu));
+  }
+}
+initToggleMenu();
